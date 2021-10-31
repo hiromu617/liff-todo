8229f2cd3e127a559ed8025f102302c64717bcc5
hiro <hiromhiro617@gmail.com>
2021-10-31 22:41:49 +0900

    :pencil2: fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import liff from "@line/liff";
+// import liff from "@line/liff";
 import "./App.css";
 import { TodoItemList } from "./components/TodoItemList";
 import { Item } from "./types/item.type";
@@ -11,49 +11,49 @@ const TodoItems: Item[] = [
 ];
 
 function App() {
-  const sendMessage = () => {
-    liff
-      .init({ liffId: process.env.REACT_APP_LIFF_ID as string }) // LIFF IDをセットする
-      .then(() => {
-        if (!liff.isLoggedIn()) {
-          liff.login({}); // ログインしていなければ最初にログインする
-        } else if (liff.isInClient()) {
-          // LIFFので動いているのであれば
-          liff
-            .sendMessages([
-              {
-                // メッセージを送信する
-                type: "text",
-                text: "You've successfully sent a message! Hooray!",
-              },
-            ])
-            .then(function () {
-              window.alert("Message sent");
-            })
-            .catch(function (error) {
-              window.alert("Error sending message: " + error);
-            });
-        }
-      });
-  };
-  const getUserInfo = () => {
-    liff.init({ liffId: process.env.REACT_APP_LIFF_ID as string }).then(() => {
-      if (!liff.isLoggedIn()) {
-        liff.login({}); // ログインしていなければ最初にログインする
-      } else if (liff.isInClient()) {
-        liff
-          .getProfile() // ユーザ情報を取得する
-          .then((profile) => {
-            const userId: string = profile.userId;
-            const displayName: string = profile.displayName;
-            alert(`Name: ${displayName}, userId: ${userId}`);
-          })
-          .catch(function (error) {
-            window.alert("Error sending message: " + error);
-          });
-      }
-    });
-  };
+  // const sendMessage = () => {
+  //   liff
+  //     .init({ liffId: process.env.REACT_APP_LIFF_ID as string }) // LIFF IDをセットする
+  //     .then(() => {
+  //       if (!liff.isLoggedIn()) {
+  //         liff.login({}); // ログインしていなければ最初にログインする
+  //       } else if (liff.isInClient()) {
+  //         // LIFFので動いているのであれば
+  //         liff
+  //           .sendMessages([
+  //             {
+  //               // メッセージを送信する
+  //               type: "text",
+  //               text: "You've successfully sent a message! Hooray!",
+  //             },
+  //           ])
+  //           .then(function () {
+  //             window.alert("Message sent");
+  //           })
+  //           .catch(function (error) {
+  //             window.alert("Error sending message: " + error);
+  //           });
+  //       }
+  //     });
+  // };
+  // const getUserInfo = () => {
+  //   liff.init({ liffId: process.env.REACT_APP_LIFF_ID as string }).then(() => {
+  //     if (!liff.isLoggedIn()) {
+  //       liff.login({}); // ログインしていなければ最初にログインする
+  //     } else if (liff.isInClient()) {
+  //       liff
+  //         .getProfile() // ユーザ情報を取得する
+  //         .then((profile) => {
+  //           const userId: string = profile.userId;
+  //           const displayName: string = profile.displayName;
+  //           alert(`Name: ${displayName}, userId: ${userId}`);
+  //         })
+  //         .catch(function (error) {
+  //           window.alert("Error sending message: " + error);
+  //         });
+  //     }
+  //   });
+  // };
 
   return (
     <div className="bg-blue-50 w-full min-h-screen px-5">
