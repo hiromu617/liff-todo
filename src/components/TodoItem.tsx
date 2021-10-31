aea9847efa0ae9ce851149b661c970efac15fbd6
hiro <hiromhiro617@gmail.com>
2021-10-31 14:34:21 +0900

    :lipstick: top ui

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,15 +1,24 @@
-import React from "react"
-import { Item } from "../types/item.type"
+import React from "react";
+import { Item } from "../types/item.type";
+import { CheckIcon } from "@heroicons/react/solid";
 
 export type TodoItemProps = {
-  item: Item
-}
+  item: Item;
+};
 
-export const TodoItem: React.VFC<TodoItemProps> = ({item}) => {
+export const TodoItem: React.VFC<TodoItemProps> = ({ item }) => {
   return (
     <div className="flex gap-5 justify-start items-center bg-white p-5 rounded-lg shadow-md">
-      <button>{item.finished ? "close" : "open"}</button>
+      <button>
+        <CheckIcon
+          className={`h-6 w-6 ${
+            item.finished
+              ? "text-blue-500"
+              : "text-blue-100 hover:text-blue-300"
+          }`}
+        />
+      </button>
       <label className="font-semibold text-lg">{item.title}</label>
     </div>
-  )
-}
+  );
+};
--- src/components/TodoItemList.tsx
+++ src/components/TodoItemList.tsx
@@ -1,15 +1,37 @@
-import React from "react"
-import { Item } from "../types/item.type"
-import {TodoItem} from "./TodoItem"
+import React from "react";
+import { Item } from "../types/item.type";
+import { TodoItem } from "./TodoItem";
+import { PlusIcon } from "@heroicons/react/solid";
 
 export type TodoItemListProps = {
-  items: Item[]
-}
+  items: Item[];
+};
 
-export const TodoItemList: React.VFC<TodoItemListProps> = ({items}) => {
+export const TodoItemList: React.VFC<TodoItemListProps> = ({ items }) => {
+  const finishedItem = items.filter((item) => item.finished);
+  const notYetFinishedItem = items.filter((item) => !item.finished);
+
   return (
-    <div className="py-20 flex flex-col gap-5">
-    {items.map((item) => <TodoItem item={item} />)}
+    <div className="py-20">
+      <div className="mb-5 text-center">
+        <button className="flex items-center justify-center gap-2 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 w-full border-b-4 border-blue-700 hover:border-blue-500 rounded">
+          <PlusIcon className="h-5 w-5 text-white " />
+          New Todo
+        </button>
+      </div>
+      <div className="flex flex-col gap-5">
+        {notYetFinishedItem.map((item) => (
+          <TodoItem item={item} />
+        ))}
+      </div>
+      <div className="bg-blue-400 px-5 py-2 my-5 rounded">
+        <h2 className="text-xl font-semibold text-white">Finished</h2>
+      </div>
+      <div className="flex flex-col gap-5">
+        {finishedItem.map((item) => (
+          <TodoItem item={item} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
