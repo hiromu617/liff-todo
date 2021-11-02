7185a845e1c53f46913e2517bc9734a24ececa95
hiro <hiromhiro617@gmail.com>
2021-11-02 12:48:48 +0900

    :bug: fix env

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 
-const AXIOS_BASE_URL = process.env.AXIOS_BASE_URL;
+const AXIOS_BASE_URL = process.env.REACT_APP_AXIOS_BASE_URL;
 
 export const client = axios.create({
   baseURL: AXIOS_BASE_URL || "http://localhost:3000",
