f0ec31c90a42a0aa091eae2061e72da0da88b7e2
hiro <hiromhiro617@gmail.com>
2021-11-03 15:23:56 +0900

    :sparkles: user id validation

--- src/components/EditTodoForm.tsx
+++ src/components/EditTodoForm.tsx
@@ -44,9 +44,12 @@ export const EditTodoForm: React.VFC<EditTodoFormProps> = ({
     }
   };
 
+  // <form>の中に入れるとupdateTodoItemも走ってしまう
   const deleteTodoItem = async () => {
     try {
-      const res = await client.delete(`/item/${TodoItem.id}`);
+      const res = await client.post(`/item/${TodoItem.id}/delete`, {
+        user_id: userId,
+      });
       console.log(res.data);
       revalidate();
     } catch (e) {
@@ -57,53 +60,57 @@ export const EditTodoForm: React.VFC<EditTodoFormProps> = ({
   };
 
   return (
-    <form onSubmit={updateTodoItem}>
-      <div className="mt-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2">
-          Title
-        </label>
-        <input
-          value={formData.title}
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline disabled:text-gray-700 disabled:shadow-none disabled:border-transparent disabled:bg-white"
-          type="text"
-          name={formData.title}
-          placeholder="Todo title"
-          onChange={(e) => setFormData({ ...formData, title: e.target.value })}
-          disabled={!isEditable}
-        />
-      </div>
-      <div className="mt-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2">
-          Description
-        </label>
-        <textarea
-          value={formData.description}
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline disabled:text-gray-700 disabled:shadow-none disabled:border-transparent disabled:bg-white"
-          name="description"
-          placeholder="Todo Description"
-          onChange={(e) =>
-            setFormData({ ...formData, description: e.target.value })
-          }
-          disabled={!isEditable}
-        />
-      </div>
-      <div className="mt-4 flex justify-end items-center gap-5">
-        {isEditable && (
-          <button
-            type="submit"
-            className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-            disabled={isSubmitting}
-          >
-            {isSubmitting ? "Submitting..." : "Save"}
-          </button>
-        )}
-        <button
-          onClick={() => deleteTodoItem()}
-          className="inline-flex justify-center px-4 py-2 text-sm font-medium text-red-900 bg-red-100 border border-transparent rounded-md hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500"
-        >
-          Delete
-        </button>
-      </div>
-    </form>
+    <>
+      <button
+        onClick={() => deleteTodoItem()}
+        className="inline-flex justify-center px-4 py-2 text-sm font-medium text-red-900 bg-red-100 border border-transparent rounded-md hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500"
+      >
+        Delete
+      </button>
+      <form onSubmit={updateTodoItem}>
+        <div className="mt-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Title
+          </label>
+          <input
+            value={formData.title}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline disabled:text-gray-700 disabled:shadow-none disabled:border-transparent disabled:bg-white"
+            type="text"
+            name={formData.title}
+            placeholder="Todo title"
+            onChange={(e) =>
+              setFormData({ ...formData, title: e.target.value })
+            }
+            disabled={!isEditable}
+          />
+        </div>
+        <div className="mt-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Description
+          </label>
+          <textarea
+            value={formData.description}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline disabled:text-gray-700 disabled:shadow-none disabled:border-transparent disabled:bg-white"
+            name="description"
+            placeholder="Todo Description"
+            onChange={(e) =>
+              setFormData({ ...formData, description: e.target.value })
+            }
+            disabled={!isEditable}
+          />
+        </div>
+        <div className="mt-4 flex justify-end items-center gap-5">
+          {isEditable && (
+            <button
+              type="submit"
+              className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "Submitting..." : "Save"}
+            </button>
+          )}
+        </div>
+      </form>
+    </>
   );
 };
